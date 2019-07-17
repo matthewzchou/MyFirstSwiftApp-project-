e7046035cf449b2f25d12ebb00ab573b1203d693
Matthew Chou <matthewzchou@gmail.com>
2019-07-17 14:51:13 -0700

    working dice roll

--- Dice Roll/Dice Roll/ViewController.swift
+++ Dice Roll/Dice Roll/ViewController.swift
@@ -9,16 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var ImageView1: UIImageView!
     @IBOutlet weak var imageView2: UIImageView!
-    
+    var showFirstImage: Bool = false
+    var die = ["Dice Red 1","Dice Red 2", "Dice Red 3", "Dice Red 4","Dice Red 5", "Dice Red 6"]
+    var die1 = ["Dice Blue 1","Dice Blue 2", "Dice Blue 3","Dice Blue 4","Dice Blue 5","Dice Blue 6"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func rollDice(_ sender: Any) {
-        
+        let rndNum = Int.random(in: 0...5)
+        let rndNum2 = Int.random(in:0...5)
+        ImageView1.image = UIImage(named :die[rndNum])
+        imageView2.image = UIImage(named :die1[rndNum2])
     }
     
 }
---
+++ Dice Roll/Dice Roll/secondDiceViewController.swift
@@ -0,0 +1,28 @@
+//
+//  secondDiceViewController.swift
+//  Dice Roll
+//
+//  Created by Chou on 7/17/19.
+//  Copyright © 2019 Matthew. All rights reserved.
+//
+
+import UIKit
+
+class secondDiceViewController: UIViewController {
+
+    @IBOutlet weak var imageView1: UIImageView!
+    var showfirstImage: Bool = false
+    
+    var dice = ["Red Die 1","Red Die 2","Red Die 3","Red Die 4","Red Die 5","Red Die 6"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func rollDie(_ sender: Any) {
+        let rndNum = Int.random(in: 0...5)
+        
+        imageView1.image = UIImage(named : dice[rndNum])
+    }
+}
